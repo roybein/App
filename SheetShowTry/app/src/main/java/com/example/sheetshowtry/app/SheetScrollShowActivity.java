c587e2ab3e30f036427e4709c5d04f630707db24
bianrh <roybein@gmail.com>
2014-09-01 23:32:20 +0800

    tmp

--- SheetShowTry/app/src/main/java/com/example/sheetshowtry/app/SheetScrollShowActivity.java
+++ SheetShowTry/app/src/main/java/com/example/sheetshowtry/app/SheetScrollShowActivity.java
@@ -4,9 +4,9 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.graphics.Point;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,21 +26,16 @@ public class SheetScrollShowActivity extends Activity {
         bitmap = BitmapFactory.decodeFile(sampleSheetFile.getPath());
         sheetScrollShowView = (ImageView) findViewById(R.id.sheet_scroll_image_view);
         sheetScrollShowView.setImageBitmap(bitmap);
-        sheetScrollShowView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                ImageView imageView = (ImageView) v;
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-                        matrix
-                }
-            }
-        });
+        Matrix matrix = new Matrix();
+        matrix.set(sheetScrollShowView.getImageMatrix());
+        matrix.postScale(2, 2);
+        sheetScrollShowView.setImageMatrix(matrix);
+        sheetScrollShowView.setOnTouchListener(new ImageShowTouchListener());
+        sheetScrollShowView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
     }
 }
 
-public class ImageShowTouchListener implements View.OnTouchListener {
+class ImageShowTouchListener implements View.OnTouchListener {
     private Matrix matrix = new Matrix();
     private PointF startPoint = new PointF();
 
@@ -50,16 +45,21 @@ public class ImageShowTouchListener implements View.OnTouchListener {
 
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
+                Log.i(null, "ACTION_DOWN");
                 matrix.set(view.getImageMatrix());
                 startPoint.set(event.getX(), event.getY());
                 break;
-            case MotionEvent.ACTION_UP;
-
+            case MotionEvent.ACTION_UP:
+                Log.i(null, "ACTION_UP");
                 break;
-            case MotionEvent.ACTION_MOVE;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(null, "ACTION_MOVE");
+                matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
                 break;
         }
 
+        Log.i(null, "view set image matrix" + matrix.toString());
         view.setImageMatrix(matrix);
+        return true;
     }
 }
